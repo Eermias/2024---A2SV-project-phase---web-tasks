4968f19ec2eb27bfa2b98444cb3f26060f13a396
Ermias Abathun <abathunermias@gmail.com>
2024-08-19 09:46:55 +0300

    Add Task - 1 folder

---
+++ Task - 1/script.js
@@ -0,0 +1,14 @@
+function addTask() {
+    const taskInput = document.getElementById('taskInput');
+    const taskList = document.getElementById('taskList');
+
+    if (taskInput.value.trim() !== "") {
+        const li = document.createElement('li');
+        li.textContent = taskInput.value;
+        li.onclick = function() {
+            taskList.removeChild(li);
+        };
+        taskList.appendChild(li);
+        taskInput.value = "";
+    }
+}
